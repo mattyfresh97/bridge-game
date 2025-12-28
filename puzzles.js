1fcddac045153c7ce6bd5005c6d8332bdbf68e25
Matty <mattkorzen14@gmail.com>
2025-12-27 19:42:22 -0500

    update to match main

--- puzzles.js
+++ puzzles.js
@@ -1,7 +1,5 @@
 export const DAILY_PUZZLES = [
-  { start: "bingo", end: "card" },  // Day 0
+  { start: "brain", end: "storm" },  // Day 0
   { start: "apple", end: "sauce" },  // Day 1
-  { start: "sugar", end: "spice" },  // Day 2
-  { start: "storm", end: "cloud" },   // Day 2
   { start: "lemon", end: "water" }   // Day 2
 ];
