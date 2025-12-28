d9e994d2809240ae267de596b090cd9de356f8ba
Matty <mattkorzen14@gmail.com>
2025-12-27 23:47:40 -0500

    updated, fixed minor bugs

--- puzzles.js
+++ puzzles.js
@@ -1,8 +1,8 @@
 export const DAILY_PUZZLES = [
-  {start: "soccer", end: "goal"},
+  {start: "brain", end: "storm" },  // Day 0
   {start: "yoga", end: "pose"},
   {start: "lucky", end: "charm"},
-  { start: "brain", end: "storm" },  // Day 0
   { start: "apple", end: "sauce" },  // Day 1
   { start: "lemon", end: "water" }   // Day 2
+  {start: "soccer", end: "goal"},
 ];
