11146162bcfed8e691ea51faa0f9ecf21b55e9fb
Matty <mattkorzen14@gmail.com>
2025-12-28 22:07:10 -0500

    Initialized puzzle for sharing, updated share text

--- puzzles.js
+++ puzzles.js
@@ -1,8 +1,9 @@
 export const DAILY_PUZZLES = [
-  {start: "brain", end: "storm" },  // Day 0
+  {start: "apple", end: "sauce"},  // Day 1
   {start: "yoga", end: "pose"},
   {start: "lucky", end: "charm"},
-  { start: "apple", end: "sauce" },  // Day 1
-  { start: "lemon", end: "water" },   // Day 2
-  {start: "soccer", end: "goal"},
+  {start: "brain", end: "storm"},  // Day 0
+  {start: "snow", end: "flake"},  // Day 0
+  {start: "lemon", end: "water"},   // Day 2
+  {start: "soccer", end: "goal"}
 ];
